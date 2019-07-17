b6493478b41d4b7b69e5fd13fe7326e22bb9038c
Mahendra Singh <mahendra.life@hotmail.com>
2019-07-17 23:26:58 +0530

    Update language-translate.component.ts

--- src/app/component/language-translate/language-translate.component.ts
+++ src/app/component/language-translate/language-translate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageList } from '../../translatorConfig';
+import { dropdownList , getDropDown} from './dropdown';
 
 @Component({
   selector: 'app-language-translate',
@@ -8,13 +9,51 @@ import { LanguageList } from '../../translatorConfig';
   styleUrls: ['./language-translate.component.scss'],
 
 })
+
 export class LanguageTranslateComponent implements OnInit {
-  LanguageList =LanguageList;
-  param = {value: 'world'};
+
+  //For Language English and Italic
+  LanguageList = LanguageList;
+  param = { value: 'world' };
+
+
+  //For Multi Select
+  dropdownList = dropdownList;
+  selectedItems = [];
+  dropdownSettings = {};
+
   constructor(public translate: TranslateService) {
+    this.dropdownList = getDropDown(translate.defaultLang);
+    // if user can chnage seleced language 
+    translate.onLangChange.subscribe( lng => { 
+      this.dropdownList = getDropDown(lng['lang']);
+    });
+
   }
 
+
   ngOnInit() {
+  
+    this.selectedItems = [
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' }
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
   }
+    onItemSelect(item: any) {
+      console.log(item);
+    }
+    onSelectAll(items: any) {
+      console.log(items);
+    }
 
 }
